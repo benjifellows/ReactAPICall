4faecd64bb611d35e55e13035b5865b34645ab61
Ben Fellows <benjamin.fellows@bbc.co.uk>
2019-10-30 15:48:49 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import { makeStyles, useTheme } from '@material-ui/core/styles'
+
+
+
+function Button(props) {
+  // const handleClick = () => setCounter(counter+1);
+
+	return (
+  	<button className="Button" onClick={props.onClickFunction}>
+      Call API
+    </button>
+  );
+}
+
+function Display(props) {
+	return (
+  	<div>{props.message}</div>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	const [response, setResponse] = React.useState(0);
+  const makeRequest = () => setResponse(
+        fetch('https://7h1a1zzgya.execute-api.eu-west-1.amazonaws.com/default/HelloBenji')
+        );
+	return (
+    <div className = "App">
+      <Button onClickFunction={makeRequest}  />
+
+      <Display message={response}/>
     </div>
   );
 }
+
+
 
 export default App;
