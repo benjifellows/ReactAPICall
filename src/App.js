78252e357457d85d49891112a84ede2c625615ef
Ben Fellows <benjamin.fellows@bbc.co.uk>
2019-10-31 09:41:55 +0000

    fetch added

--- src/App.js
+++ src/App.js
@@ -20,11 +20,12 @@ function Display(props) {
   );
 }
 
+
 function App() {
 	const [response, setResponse] = React.useState(0);
-  const makeRequest = () => setResponse(
+  const makeRequest = () =>
         fetch('https://7h1a1zzgya.execute-api.eu-west-1.amazonaws.com/default/HelloBenji')
-        );
+        ;
 	return (
     <div className = "App">
       <Button onClickFunction={makeRequest}  />
